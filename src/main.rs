03bb216f01335ca5a082787507c76c9749815e66
Joe Becher <me@drazisil.com>
2020-11-17 08:29:54 -0500

    first test

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 #![no_std]
 #![no_main]
-
 #![feature(ptr_internals)]
+
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 #[macro_use]
 mod vga;
@@ -13,6 +16,9 @@ pub extern "C" fn _start() -> ! {
 
     println!("Hello, kernel!");
 
+    #[cfg(test)]
+    test_main();
+
     println!("Did not crash!");
 
     loop {}
@@ -22,6 +28,22 @@ use core::panic::PanicInfo;
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
